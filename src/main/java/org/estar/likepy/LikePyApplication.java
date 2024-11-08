be5df34a7f1587dbf14d06dfb9b76784a9eb1164
Estar0622 <myjudai@naver.com>
2024-11-09 01:55:45 +0900

    LikePy

---
+++ src/main/java/org/estar/likepy/LikePyApplication.java
@@ -0,0 +1,13 @@
+package org.estar.likepy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikePyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikePyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/likepy/LikePyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.likepy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikePyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
